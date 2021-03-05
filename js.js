107772937a7ce33c4bc48fd192a7e14e55abf786
EORIN <73447841+EORIN@users.noreply.github.com>
2021-03-05 16:08:24 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,44 +1,45 @@
 
-//let heroes = [{name: 'Аня', img:'qq.png'},{name: 'Ника', img:'ly.png'}]
-class heroesT {
-    constructor (name, img){
-        this.name = name
-        this.img = img
-    }
-}
-//let Anya = new heroesT ('Аня', 'ly.png')
 let scenarii = [
-    ['привет1','Аня', 'ly.png'],
-    ['привет2','Ника', 'qq.png'],
-    ['привет3','Аня', 'ly.png'],
-    ['привет4','Польнорефф', 'pol.jpg'],
-    ['привет5','Ника', 'qq.png'],
-    ['привет6','Аня', 'ly.png'],
+    ['привет1','Аня', '1.png', ['1','2']],
+    ['привет2','Ника', '2.png'],
+    ['привет3','Аня', '1.png'],
+    ['привет4','Польнорефф', '3.png'],
+    ['привет5','Ника', '2.png'],
+    ['привет6','Аня', '1.png'],
 ]
 
 let click = 0
 
+
+
+
 function check (){
+   
+ 
     let elem = document.getElementById(`div${scenarii[click][1]}`)
     $('#botName').empty()
     $('#botName').append(`${scenarii[click][1]}`)
     $('#botText').empty()
     $('#botText').append(`${scenarii[click][0]}`)
-
+    
     if (elem == null) {  
    $('#top').append(`<img src="${scenarii[click][2]}" id="div${scenarii[click][1]}">`)}
+/*   let elemT = document.getElementById(`div${scenarii[click][1]}`)
+let size = 300
+   let timer = setInterval(function(){
+   if (size == 350){clearInterval(timer)}
+ else {
+       size ++;
+    elemT.style.height = size + "px";
+   
+   }})*/
+   
+
+   
+ console.log(click)
    click = click + 1
-   console.log(elem)
+  
 }
 
-//function next(){
-//    if (document.getElementById("div"scenarii[click][2]"") = false)
-//}
-
-//function gg() {
-//    document.getElementById(`div${scenarii[click][2]}`) 
-//}
-//gg()
-let scena = document.getElementById('scena')
 
 
